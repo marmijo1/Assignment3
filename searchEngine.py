7a6862c8bd4111e4277362268d32e5ae8fde89b9
Michael Armijo <marmijo1@uci.edu>
2024-11-15 12:50:10 -0800

    anthony's changes

--- searchEngine.py
+++ searchEngine.py
@@ -6,15 +6,9 @@ from collections import defaultdict#for create_inverted_index
 from nltk.stem import PorterStemmer
 
 all_tokens = []  #global
-<<<<<<< HEAD
 inverted_index = defaultdict(list) #tdefault dict  will handle missing keys if necessary
 
 stemmer = PorterStemmer() # Intialize Porter Stemmer
-=======
-
-from collections import defaultdict #for create_inverted_index
-
->>>>>>> edfa1e777f49020342ab53b13c71343f6209565c
 # Used to load the content from a specified directory
 def load_content(directory):
     documents = [] #Empty list to store the documents' data 
@@ -44,7 +38,6 @@ def load_content(directory):
     return documents
 
 #tokenizer (needs work,, add filters?)
-<<<<<<< HEAD
 def tokenize_text_and_stem(text):
     # tokens = []
     # excluded_terms = {} #include any excluded terms here <<-------
@@ -68,45 +61,17 @@ def tokenize_text_and_stem(text):
 #     all_tokens.extend(new_tokens) #this will hold the tokens permanently. NOte: May need to clear memory after runs, depending on how we implement this
     #I figured we can store the inverted index into a JSON file for better memory usage (when looking
     #at the porter stemmer on various websites like geeksforgeeks.org suggested keeping inverted index in file)
-=======
-def tokenize_text(text):
-    tokens = []
-    excluded_terms = {} #include any excluded terms here <<-------
-
-    # Find sequences of alphabet characters that are at least 2 characters long
-    raw_tokens = re.findall(r'[a-zA-Z]{2,}', text)
-
-    # Filter out unwanted tokens
-    for token in raw_tokens:
-        normalized_token = token.lower()
-        if normalized_token not in excluded_terms:
-            tokens.append(normalized_token)
-    
-    # Update the master token list
-    append_to_token_list(tokens)
-    return tokens
-
-def append_to_token_list(new_tokens): #helper for tokenizer. You can leave this alone
-    global all_tokens
-    all_tokens.extend(new_tokens) #this will hold the tokens permanently. NOte: May need to clear memory after runs, depending on how we implement this
-
->>>>>>> edfa1e777f49020342ab53b13c71343f6209565c
 
 #inv idx creation 
 def create_inverted_index(documents):
 
-<<<<<<< HEAD
 
-=======
-    inverted_index = defaultdict(list) #tdefault dict  will handle missing keys if necessary
->>>>>>> edfa1e777f49020342ab53b13c71343f6209565c
     
     #Implementation Required
 
     for document in documents: #iterate through documents list 
         file_name = document['filename'] #pull from load content use type filename and type content
         content = document['content']
-<<<<<<< HEAD
         if content:
             print(
                 f"Document: {document['filename']}, Content length: {len(content)}")  # Verify content length
@@ -114,12 +79,6 @@ def create_inverted_index(documents):
         term_frequency = defaultdict(int)  #will also handle missing keys
 
         tokens = tokenize_text_and_stem(content)
-=======
-
-        term_frequency = defaultdict(int)  #will also handle missing keys
-
-        tokens = tokenize_text(content)
->>>>>>> edfa1e777f49020342ab53b13c71343f6209565c
         for token in tokens: #iterate through the tokens
             term_frequency[token] += 1 #increment term frequency of a found token per loop
 
@@ -134,7 +93,6 @@ def create_inverted_index(documents):
         
     return inverted_index
 
-<<<<<<< HEAD
 #Save the inverted index to a JSON file
 def saveInvertedIndex(file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
@@ -146,8 +104,6 @@ def save_report(file_path, numDocuments, numUniqueTokens, indexSizeKB):
         file.write(f"Number of unique tokens: {numUniqueTokens}\n")
         file.write(f"Total size of the index on disk (KB): {indexSizeKB}\n")
 
-=======
->>>>>>> edfa1e777f49020342ab53b13c71343f6209565c
 #for output file, reading the terminal with this many lines frickin sux 
 def write_to_file(inverted_index, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
@@ -159,7 +115,6 @@ def write_to_file(inverted_index, output_file):
 
 
 #This is just a trial run that I tried to see if it's parsing through the information correctly. 
-<<<<<<< HEAD
 # data_directory = os.path.join(os.path.dirname(__file__), 'ANALYST', 'www_cs_uci_edu')
 # documents_test = load_content(data_directory)
 # print(f"loaded {len(documents_test)} documents")
@@ -193,18 +148,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-data_directory = os.path.join(os.path.dirname(__file__), 'ANALYST', 'www_cs_uci_edu')
-documents_test = load_content(data_directory)
-print(f"loaded {len(documents_test)} documents")
-
-#Create the inverted index
-inverted_index = create_inverted_index(documents_test) #using documents_test)
-
-
-
-output_file_path = os.path.join(os.path.dirname(__file__), 'invertedIDX_output.txt')
-write_to_file(inverted_index, output_file_path)
-
-print(f"Inverted index written to {output_file_path}")
->>>>>>> edfa1e777f49020342ab53b13c71343f6209565c
